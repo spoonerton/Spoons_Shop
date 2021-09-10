13a71249bcd6bc59a3fd7e3b80073ff4f1659592
spoonerton <spoonerton@gmail.com>
2021-09-10 02:50:20 -0400

    ShopUpdates

--- customJS.js
+++ customJS.js
@@ -1,6 +1,7 @@
 const itemListURL = "https://raw.githubusercontent.com/spoonerton/Spoons_Shop/main/StoreItems.json";
 const eventListURL = "https://raw.githubusercontent.com/spoonerton/Spoons_Shop/main/StoreIncidents.json";
 const shopExtURL = "https://raw.githubusercontent.com/spoonerton/Spoons_Shop/main/ShopExt.json";
+const modListURL = "https://raw.githubusercontent.com/spoonerton/Spoons_Shop/main/modlist.json"
 
 let itemList = null;
 $.getJSON(itemListURL, function(data) {
@@ -16,6 +17,10 @@ $.getJSON(itemListURL, function(data) {
         }
     });
 
+    if (itemData.length == 0){
+        document.getElementById('items').prepend("Items are Disabled!");
+        $("#items").children().hide();
+    }
     itemList = new List('items', options, itemData);
 });
 
@@ -45,25 +50,27 @@ $.getJSON(eventListURL, function(data) {
 
 let traitList = null;
 let raceList = null;
-$.getJSON(shopExtURL, function(data) {  
-    //TraitList  
-    var options = {
-        valueNames: ['name', 'addPrice', 'removePrice'],
-        item: '<tr><td class="name" scope="row"></td><td class="addPrice"></td><td class="removePrice"></td></tr>'
+$.getJSON(shopExtURL, function(data) {
+
+//TraitList
+    var items = '';
+    $.each(data.traits, function(index, val){
+        if (val.canAdd == true){        
+            items += '<tr>';
+            items += '<td class="name"><b>' + val.name + '</b></br><small>' + val.description + '</small></td>';
+            items += '<td class="stats">' + val.stats + '</td>';
+            items += '<td class="addPrice">' + val.addPrice + '</td>';
+            items += '<td class="removePrice">' + val.removePrice + '</td>';
+            items += '</tr>';
+        }        
+    })
+    $('#traitstable').append(items);
+ 
+  var options = {
+     valueNames: ['name', 'addPrice', 'removePrice', 'stats','canAdd'],
     };
-    var traitData = [];
-    $.each(data.traits, function(index, value) {
-        var traitBlacklist = ['robot'];
-        if (traitBlacklist.indexOf(value["name"]) != -1){
-            return;
-        } 
+    traitList = new List('traits', options);
 
-        if (value["addPrice"] > 0) {
-            traitData.push(value);
-        }
-    });
-
-    traitList = new List('traits', options, traitData);
 
     //RaceList
     options = {
@@ -89,3 +96,25 @@ var tabs = angular.module('tabs', [])
     .controller('tabCtrl', function($scope) {
         $scope.selected = "1";
     });
+
+    function searchBar() {
+        // Declare variables
+        var input, filter, table, tr, td, i, txtValue;
+        input = document.getElementById("myInput");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("myTable");
+        tr = table.getElementsByTagName("tr");
+      
+        // Loop through all table rows, and hide those who don't match the search query
+        for (i = 0; i < tr.length; i++) {
+          td = tr[i].getElementsByTagName("td")[0];
+          if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+              tr[i].style.display = "";
+            } else {
+              tr[i].style.display = "none";
+            }
+          }
+        }
+      }
